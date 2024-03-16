cc93a32d5af209526cb93359f6812b8be14c225f
igor <igor@groql.io>
2024-03-16 12:25:47 +0700

    example router

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -1,12 +1,28 @@
+from backend.db import get_db_session
+from backend.model import LogEntry
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from backend.db import get_db_session
-from backend.model import LogEntry
+from auth import router as auth_router
+from example.router import router as example_router
+from conf import LOGIN_CALLBACK_URI
 
 app = FastAPI()
 
+origins = [
+    LOGIN_CALLBACK_URI
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["POST", "PUT", "GET", "DELETE"],
+    allow_headers=["*"],
+    max_age=3600,
+)
 
 @app.get("/")
 async def root(session: AsyncSession = Depends(get_db_session)):
@@ -22,3 +38,7 @@ async def some_post(text: str, session: AsyncSession = Depends(get_db_session)):
     query = insert(LogEntry).values(note=text)
     await session.execute(query)
     await session.commit()
+
+
+app.include_router(auth_router)
+app.include_router(example_router)
