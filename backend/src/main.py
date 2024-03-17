56f325ede49bb970e3dcdd5e07727850716450ba
TJesT <alexeynospid@gmail.com>
2024-03-17 21:09:48 +0700

    + /docs /healthcheck

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.openapi.docs import get_swagger_ui_html
 
 from src.auth.routes import router as auth_router
 from src.api.routers import example_router, log_router
@@ -8,13 +9,13 @@ from src.core.db import init_db
 from src.auth.core import get_user, create_user
 from src.mqtt import init_mqtt
 from src.mqtt.example import mqtt_example_router
-from src.schemas.user import User, RoleEnum
+from src.schemas.user import RoleEnum
 
 
 async def create_user_if_not_exists(name: str, password: str, roles: list[RoleEnum]):
     user = await get_user(name)
     if user is None:
-        print(f'Creating user {name}')
+        print(f"Creating user {name}")
         await create_user(name, password, roles)
 
 
@@ -22,8 +23,8 @@ async def create_user_if_not_exists(name: str, password: str, roles: list[RoleEn
 async def lifespan(app: FastAPI):
 
     await init_db()
-    await create_user_if_not_exists('admin', '123', [RoleEnum.ADMIN])
-    await create_user_if_not_exists('ilya', 'qwe', [])
+    await create_user_if_not_exists("admin", "123", [RoleEnum.ADMIN])
+    await create_user_if_not_exists("ilya", "qwe", [])
     await init_mqtt()
     yield
     pass
@@ -35,3 +36,13 @@ app.include_router(log_router)
 app.include_router(auth_router)
 app.include_router(example_router)
 app.include_router(mqtt_example_router)
+
+
+@app.get("/healthcheck")
+async def healthcheck():
+    return {"status": "ok"}
+
+
+@app.get("/docs", include_in_schema=False)
+async def swagger_ui_html():
+    return get_swagger_ui_html(openapi_url="/openapi.json", title="NTI-API docs")
