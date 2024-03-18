129655eaf80c5b982280e7b96dbb3abc742bee7b
TJesT <alexeynospid@gmail.com>
2024-03-19 02:54:09 +0700

    cors added

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.auth.routes import router as auth_router
 from src.api.routers import example_router, log_router
@@ -27,6 +28,13 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(log_router)
 app.include_router(auth_router)
