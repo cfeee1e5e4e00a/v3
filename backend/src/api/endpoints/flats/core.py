9e42ee139838537fc7225b2cd3d279feccdd4cae
TJesT <alexeynospid@gmail.com>
2024-03-19 19:44:53 +0700

    first influx endpoint with schemas

--- backend/src/api/endpoints/flats/core.py
+++ backend/src/api/endpoints/flats/core.py
@@ -1,21 +1,26 @@
 from fastapi import Depends, APIRouter
 from influxdb_client import QueryApi
+from itertools import chain
+
 from src.core.db import get_influx_query
+from src.schemas.influx import SensorData
 
 router = APIRouter(prefix="/flats")
 
 
-@router.get("/{id}/temp")
-async def root(id: int, period: str, query_api: QueryApi = Depends(get_influx_query)):
+@router.get("/{id}/temp", response_model=list[SensorData[float]])
+async def get_temperatures(
+    id: int, period: str, query_api: QueryApi = Depends(get_influx_query)
+):
     query = f"""from(bucket: "default")\
     |> range(start: -{period})\
     |> filter(fn: (r) => r["_measurement"] == "temp")\
     |> filter(fn: (r) => r["_field"] == "value")\
     |> yield(name: "last")"""
 
-    obj = query_api.query(query)
+    tables = query_api.query(query)
 
-    print(obj, type(obj), f"{obj!r}")
-    print(dir(obj))
-
-    return obj
+    return map(
+        SensorData[float].from_flux_record,
+        chain.from_iterable(table.records for table in tables),
+    )
