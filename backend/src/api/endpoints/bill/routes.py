1d3349dc1aa5794ea06f89f9b8a04e92aeaf486a
igor <igor@groql.io>
2024-03-20 17:48:56 +0700

    billing without pdf

--- backend/src/api/endpoints/bill/routes.py
+++ backend/src/api/endpoints/bill/routes.py
@@ -1,41 +1,34 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 
-from src.models.user import Role
-from src.api.endpoints.auth.core import auth_user, create_access_token, current_user
-from src.schemas.bill import AddBillRequest
-from ..auth.routes import current_user
-from src.api.endpoints.bill.core import create_bill, Status
-
+from src.api.endpoints.bill.core import create_bill, Status, get_bill, get_user_bills_list, change_bill_status
 
 router = APIRouter(prefix="/bill")
 
 
-# TODO: Выставить новый счёт юзеру
 @router.post("/")
 async def add_bill(amount: float, user_id: int):  # type: ignore
-    await create_bill(amount=amount, status=Status.UNPAID, user_id=user_id)
-    return
+    return {"bill_id": await create_bill(amount=amount, status=Status.UNPAID, user_id=user_id)}
 
 
-# TODO: Получить инфу о счёте
+# TODO: убрать pdf
 @router.get("/{bill_id}")
 async def get_bill_by_id(bill_id: int):
-    pass
+    bill = await get_bill(bill_id)
+    return bill
 
 
 # TODO: Скачать pdf-отчёт
 @router.get("/{bill_id}/report")
 async def get_bill_report(bill_id: int):
-    pass
+    bill = await get_bill(bill_id)
+    return bill.pdf
 
 
-# TODO: Получить счёта пользователя
 @router.get("/users/{user_id}")
 async def get_user_bill(user_id: int):
-    pass
+    return await get_user_bills_list(user_id)
 
 
-# TODO: "Оплатить" счёт
 @router.post("/{bill_id}/pay")
 async def pay_for_bill(bill_id: int):
-    pass
+    return await change_bill_status(bill_id)
