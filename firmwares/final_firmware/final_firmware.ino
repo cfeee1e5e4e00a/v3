754dd1e150cc135dca426d4f6793ecb9017133e8
spooky-soup <wtfdashaaa@gmail.com>
2024-03-20 13:14:34 +0700

    pid setup and calibration

--- firmwares/final_firmware/final_firmware.ino
+++ firmwares/final_firmware/final_firmware.ino
@@ -10,6 +10,7 @@
 
 #define CALIBRATION_PERIOD 1000
 #define MEASUREMENT_PERIOD 500
+#define SWITCH_PERIOD 5000
 
 
 const char* ssid = "TP-Link_4F90";
@@ -26,9 +27,21 @@ EspMQTTClient client;
 
 DHT dhts[N_ROOMS];
 ACS712 acss[N_ROOMS];
-GyverPID pids[2];
+GyverPID pids[N_ROOMS];
 
+typedef enum {
+    CONSTANT,
+    EFFECTIVE,
+    TIME_DEPENDENT,
+    OFF
+} Mode;
+Mode modes[N_ROOMS];
+float setpoints[N_ROOMS];
 
+bool relay_states[N_ROOMS*N_RELAYS];
+bool rooms_states[N_ROOMS];
+unsigned long int switch_timer = 0;
+int heat_times[N_ROOMS];
 
 void setup()
 {
@@ -67,6 +80,9 @@ void setup()
         // setup current sensors
         acss[i] = ACS712(config.rooms[i].acs_pin, 3.3, 4095, 100);
         acss[i].autoMidPointDC();
+
+        //setup current modes
+        modes[i] = OFF;
     }
 
     
@@ -74,6 +90,7 @@ void setup()
 
 void onConnectionEstablished() {
     client.publish("/init", String("power,esp=") + config.client_id + " value=1");
+    //TODO: subscribe to topic with work modes
 }
 
 volatile float room_temps[N_ROOMS];
@@ -100,10 +117,65 @@ void loop(){
         float temp = dhts[i].readTemperature();
         float hum = dhts[i].readHumidity();
         room_temps[i] = temp;
-        float curr = abs(acss[i].mA_DC(10))/1000.0;
+        float curr = acss[i].mA_DC(10)/1000.0;
+        curr = max(curr, 0.0);
 
         publish_measurement("temp", i, temp);
         publish_measurement("humd", i, hum);
         publish_measurement("curr", i, curr);
     }
+
+    for (int i = 0; i < N_ROOMS; i++) {
+        switch_relays(i);
+    }
+}
+
+int calibrate_constant_mode(int room) {
+    pids[room].input = room_temps[room];
+    int out = pids[room].getResultTimer();
+    int t;
+    if (out < 500) {
+        t = out * 10; //[мс]
+        relay_states[room * 2] = 1; //on
+        relay_states[room * 2 + 1] = 0; //off
+    } else {
+        t = 2500 + (out - 500) * 10; //[мс]
+        relay_states[room * 2] = 1;
+        relay_states[room * 2 + 1] = 1;
+    }
+    return t;
+}
+
+void setup_constant_mode(int room, float setpoint) {
+    modes[room] = CONSTANT;
+    setpoints[room] = setpoint;
+    pids[room].setpoint = setpoint;
+    calibrate_constant_mode(room);
+
+}
+
+void switch_relays(int room) {
+    float m = millis();
+    if (rooms_states[room] == 0 && m - switch_timer > SWITCH_PERIOD) {
+        //Начало отсчета - включаем все активные нагреватели
+        switch_timer = m;
+        rooms_states[room] = 1;
+        for (int i = 0; i < N_ROOMS; i++) {
+            for (int j = 0; j < N_RELAYS; j++) {
+                if (relay_states[i * N_RELAYS + j]) {
+                    digitalWrite(config.rooms[i].relay_pins[j], R_ON);
+                } else {
+                    digitalWrite(config.rooms[i].relay_pins[j], R_OFF);
+                }
+            }
+        }
+    } else if (rooms_states[room] == 1 && m - switch_timer > heat_times[room]) {
+            if (m - switch_timer > heat_times[room]) {
+                //выключаем все нагреватели в комнате
+                for (int j = 0; j < N_RELAYS; j++) {
+                    digitalWrite(config.rooms[room].relay_pins[j], R_OFF);
+                }
+        }
+    }
+
 }
