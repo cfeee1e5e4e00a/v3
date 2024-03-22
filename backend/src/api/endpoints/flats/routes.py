fefed4bc713b382717a90d6e074a707a227eb7e5
TJesT <alexeynospid@gmail.com>
2024-03-22 11:36:10 +0700

    TODO hadnler

--- backend/src/api/endpoints/flats/routes.py
+++ backend/src/api/endpoints/flats/routes.py
@@ -59,7 +59,7 @@ async def set_target_temperature(
 @router.post("/{flat}/schedule")
 async def set_flat_temp_schedule(
     flat: int,
-    user: current_user(),
+    user: current_user(), # type: ignore
     schedule: Schedule,
 ):
     async with async_session_factory() as session:
